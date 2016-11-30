8274a6339d8d3ebc0e8cebf3bce6354be9a4b3eb
ming25 <xiangming25@163.com>
2016-11-30 17:02:19 +0800

    Add files via upload

---
+++ koa_mobile_animate/app.js
@@ -0,0 +1,53 @@
+var app = require('koa')()
+  , koa = require('koa-router')()
+  , logger = require('koa-logger')
+  , json = require('koa-json')
+  , views = require('koa-views')
+  , onerror = require('koa-onerror');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+// global middlewares
+app.use(views('views', {
+  root: __dirname + '/views',
+  default: 'ejs'
+}));
+app.use(require('koa-bodyparser')());
+app.use(json());
+app.use(logger());
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(require('koa-static')(__dirname + '/public'));
+
+// routes definition
+koa.use('/', index.routes(), index.allowedMethods());
+koa.use('/users', users.routes(), users.allowedMethods());
+
+
+koa.get('/slide_img',function *(next){
+  yield this.render('/slide_img');
+});
+
+koa.get('/requestAnimationFrame',function *(next){
+  yield this.render('/requestAnimationFrame');
+});
+
+koa.get('/navRequestAnimationFrame',function *(next){
+  yield this.render('/nav_requestAnimationFrame');
+});
+
+// mount root routes  
+app.use(koa.routes());
+
+app.on('error', function(err, ctx){
+  logger.error('server error', err, ctx);
+});
+
+module.exports = app;
